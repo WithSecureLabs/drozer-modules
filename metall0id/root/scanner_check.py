e547b3277b1bdc4a73acb35ad21b81975eb6151f
metall0id <tyrone.erasmus@gmail.com>
2013-01-18 11:17:00 +0200

    Added scanner module to check whether the device is vulnerable to various public root exploits

---
+++ metall0id/root/scanner_check.py
@@ -0,0 +1,14 @@
+from mwr.droidhg.modules import common, Module
+
+class Check(Module, common.VulnerabilityScanner):
+
+    name = "Test for vulnerabilities that allow a malicious application to gain root access."
+    description = "Test for vulnerabilities that allow a malicious application to gain root access."
+    examples = ""
+    author = ["Tyrone (@mwrlabs)"]
+    date = "2013-01-14"
+    license = "MWR Code License"
+    path = ["scanner", "root"]
+
+    vulnerabilities = "exploit.root."
+    
