54ac56648ea9d99f2f31bb07ff2160d3efbd79c7
KernelErrorZA <bernard.wagner.mwr@gmail.com>
2015-03-31 09:06:25 +0200

    Added Description

--- kernelerror/tools/misc/installcert.py
+++ kernelerror/tools/misc/installcert.py
@@ -5,8 +5,8 @@ import argparse
 
 class InstallCert(Module):
 
-    name = "Install a CA cert"
-    description = ""
+    name = "Install CA certificate"
+    description = "Install a CA certificate from the host machine. Depending on the device, the users will be prompted before installing the certificate and a pin, password or pattern lock-screen needs to be configured."
     author = "Bernard Wagner"
     date = "2014-09-22"
     license = "BSD (3-clause)"
